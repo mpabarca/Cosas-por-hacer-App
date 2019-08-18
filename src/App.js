60cdf4f387ad687d6094ea7e869d1eb376a26a33
mpabarca <mpabarca@uc.cl>
2019-08-18 16:10:48 -0400

    axios incorporado al proyecto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './bootstrap.min.css';
 import Header from './components/Header';
 import NewTask from './components/NewTask';
@@ -20,6 +21,7 @@ class App extends Component{
       tasks: newTasks,
       numberTask: (this.state.numberTask) +1
     })
+
     console.log(this.state.tasks)
   }
 
